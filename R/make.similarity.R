f3289d2c2ee61e03fcb6b06c139a4875d2bd6cb5
AllanJe <jerolona@gmail.com>
2020-03-10 00:49:14 +0100

    Create make.similarity.R

---
+++ R/make.similarity.R
@@ -0,0 +1,10 @@
+make.similarity <- function(medata, similarity) {
+  N <- ncol(medata)
+  S <- matrix(rep(NA,N^2), ncol=N)
+  for(i in 1:N) {
+    for(j in 1:N) {
+      S[i,j] <- similarity(medata[,i],medata[,j])
+    }
+  }
+  S
+}
