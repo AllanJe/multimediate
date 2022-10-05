7d24e1b6e3850ec1fe11d49cd0041abaca247a0f
AllanJe <jerolona@gmail.com>
2022-10-05 14:14:54 +0200

    Delete variableLoss.R

--- R/variableLoss.R
+++
@@ -1,4 +0,0 @@
-variableLoss<- function(estimation,truth,binary=FALSE){ #returns the quadratic loss for non binary variables, the logistic one for binary variables
-  
-  return((1-binary)*sum((estimation-truth)^2)+binary*sum(-estimation*truth+log(1+exp(estimation))))
-} 
