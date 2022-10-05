b1fd7db11f436ce148986a5a141697344d4e2d29
AllanJe <jerolona@gmail.com>
2022-10-05 14:14:20 +0200

    Delete criterion.R

--- R/criterion.R
+++
@@ -1,8 +0,0 @@
-criterion <- function(w,treatment,mediators,outcome,mu,groups,tau){
-  K=dim(mediators)[2]
-  omega <- 0
-  for (k in 1:K){
-    omega <- omega + norm(as.matrix( c(w$alpha[groups$alpha==k],w$beta[groups$beta==k]) ),type="F")
-  }
-  return(f(w,treatment,mediators,outcome,tau)+mu*omega)
-}
