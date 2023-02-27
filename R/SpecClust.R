916e9cb7644c3e48f7fcaa64c87ebd216086b994
AllanJe <jerolona@gmail.com>
2023-02-27 17:12:42 +0100

    Update SpecClust.R

--- R/SpecClust.R
+++ R/SpecClust.R
@@ -1,7 +1,8 @@
 #'@import stats
 #'@importFrom graphics par
 #'@importFrom graphics barplot
-SpecClust <-function(medata,seuil=0){
+#'
+SpecClust = function(medata,seuil=0){
   S <- make.similarity(medata, similarity)
   W <- make.affinity(S, seuil)
   D <- diag(apply(W, 1, sum))
@@ -12,5 +13,5 @@ SpecClust <-function(medata,seuil=0){
   barplot(evL$values)
   k=as.numeric(readline(prompt="Nombre de cluster? "))
   Z   <- evL$vectors[,(ncol(evL$vectors)-k+1):ncol(evL$vectors)]
-  kmeans(Z,k,nstart=3)$cluster
+  return(kmeans(Z,k,nstart=3)$cluster)
 }
