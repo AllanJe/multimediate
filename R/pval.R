89cc6944ae57121bf6dc5ccd03a70f5c4461fb47
AllanJe <jerolona@gmail.com>
2023-02-27 17:12:35 +0100

    Update pval.R

--- R/pval.R
+++ R/pval.R
@@ -1,4 +1,4 @@
-pval <- function(x, xhat,seu=0){
+pval = function(x, xhat,seu=0){
   if (xhat == seu) {out <- 1}
   else {
     out <- 2 * min(sum(x > seu,na.rm=TRUE), sum(x < seu,na.rm=TRUE)) / length(x)
