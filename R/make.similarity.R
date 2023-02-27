510654f97b596d30954b669b8c09681cc3fdf9ee
AllanJe <jerolona@gmail.com>
2023-02-27 17:12:13 +0100

    Update make.similarity.R

--- R/make.similarity.R
+++ R/make.similarity.R
@@ -1,4 +1,4 @@
-make.similarity <- function(medata, similarity) {
+make.similarity = function(medata, similarity) {
   N <- ncol(medata)
   S <- matrix(rep(NA,N^2), ncol=N)
   for(i in 1:N) {
@@ -6,5 +6,5 @@ make.similarity <- function(medata, similarity) {
       S[i,j] <- similarity(medata[,i],medata[,j])
     }
   }
-  S
+  return(S)
 }
