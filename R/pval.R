cc00d579384092e9131ad688a96c0d36aa5aed5f
AllanJe <jerolona@gmail.com>
2024-01-15 20:51:07 +0100

    Update pval.R

--- R/pval.R
+++ R/pval.R
@@ -1,7 +1,14 @@
 pval = function(x, xhat,seu=0){
-  if (xhat == seu) {out <- 1}
-  else {
-    out <- 2 * min(sum(x > seu,na.rm=TRUE), sum(x < seu,na.rm=TRUE)) / length(x)
+  if (is.na(xhat)) {
+    out =NA
   }
+  else{
+    if (xhat == seu) {out <- 1}
+    else {
+      out <- 2 * min(sum(x > seu,na.rm=TRUE), sum(x < seu,na.rm=TRUE)) / length(x)
+    }
+  }
+
   return(min(out, 1))
+
 }
